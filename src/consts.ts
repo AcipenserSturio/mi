f0187e673fd63a4705c307c75d0a6da96c92c85f
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-17 13:37:04 +0300

    use config

--- src/consts.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_URL = "kala-asi.li.pona.la";
-export const SITE_TITLE = "kala Asi";
-export const SITE_DESCRIPTION = "kala Asi's personal page.";
-export const GOATCOUNTER_URL = "kala-asi-li-pona-la.goatcounter.com";
